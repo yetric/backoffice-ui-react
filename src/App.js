25c273b015095cc325fd34d65dc1060988256e42
Mattias Hising <hising@gmail.com>
2022-02-20 20:12:13 +0100

    Added Blueprint JS

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import "normalize.css/normalize.css";
+import "@blueprintjs/core/lib/css/blueprint.css";
+import "@blueprintjs/icons/lib/css/blueprint-icons.css";
+import { Button } from "@blueprintjs/core";
 
 function App() {
+  const incrementCounter = () => {
+    console.log("click");
+  };
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <Button intent="success" text="button content" onClick={incrementCounter} />
         </p>
         <a
           className="App-link"
